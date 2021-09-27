5dc473fa218ed33a1b15aa170d27dc3a5821064f
Michal9817 <65870630+Michal9817@users.noreply.github.com>
2021-09-27 13:32:49 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,12 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install mariadb-server -y 
+sudo mysql_secure_installation
+sudo mysql -u root -p
+sudo nano /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo nano /etc/mysql/my.cnf
+sudo apt install php-mysql -y
+sudo mysql -u root -p
+sudo nano /etc/apache2/apache2.conf
+
 
