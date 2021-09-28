4f3f4f7f8027ce58ac9733eb086743cb62530766
Michal987 <michal.druciak@gmail.com>
2021-09-28 10:34:13 +0100

    ee

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install mariadb-server -y
-sudo mysql -u root <<_EOF_
+sudo mysql -u root <<EOF
 UPDATE mysql.user SET Password=PASSWORD('ZAQ!2wsx') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
@@ -12,3 +12,4 @@ _EOF_
 sudo apt install php-mysql -y
 sudo apt install phpmyadmin -y
 sudo service --status-all
+echo 'koniec instalacji'
