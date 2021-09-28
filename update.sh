3782c69780e077914b5a4443ec45c83b25a12d6d
Michal987 <michal.druciak@gmail.com>
2021-09-28 08:55:28 +0100

    s

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install mariadb-server -y
-myql --user=root <<_EOF_
+sudo myql --user=root <<_EOF_
 UPDATE mysql.user SET Password=PASSWORD('${ZAQ!2wsx}') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
