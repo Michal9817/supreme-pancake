870e7195a69b0b1219d7320fbe7bf006c6369880
Michal987 <michal.druciak@gmail.com>
2021-09-28 11:28:56 +0100

    AA

--- update.sh
+++ update.sh
@@ -7,8 +7,12 @@ DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 GRANT ALL PRIVILEGES ON *.* TO 'ADMIN'@'%' IDENTIFIED BY 'ZAQ!2wsx' WITH GRANT OPTION;
+create database wagodb;
 FLUSH PRIVILEGES;
 _EOF_
+wget https://raw.githubusercontent.com/Michal9817/supreme-pancake/main/wagodb.sql
+
+
 sudo apt install php-mysql -y
 sudo apt install phpmyadmin -y
 sudo service --status-all
